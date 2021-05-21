b16113e9d6e04903ceb495bf2915c68e1359f7af
Balint Laszlo <balint.laszlo@t-systems.com>
2021-05-21 15:12:06 +0200

    Add Swagger2 and Swagger UI

--- src/main/java/hu/dtits/springmvc/controllers/CategoryController.java
+++ src/main/java/hu/dtits/springmvc/controllers/CategoryController.java
@@ -3,9 +3,12 @@ package hu.dtits.springmvc.controllers;
 import hu.dtits.springmvc.models.CategoryDTO;
 import hu.dtits.springmvc.models.CategoryListDTO;
 import hu.dtits.springmvc.services.interfaces.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Api("Category Controller")
 @RestController
 @RequestMapping(CategoryController.BASE_URL)
 public class CategoryController {
@@ -18,12 +21,14 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    @ApiOperation("Get all Categories in a list")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public CategoryListDTO getCategories() {
         return new CategoryListDTO(categoryService.getAllCategories());
     }
 
+    @ApiOperation("Get a Category by name property")
     @GetMapping("/{name}")
     @ResponseStatus(HttpStatus.OK)
     public CategoryDTO getCategoryByName(@PathVariable String name) {
--- src/main/java/hu/dtits/springmvc/models/CategoryDTO.java
+++ src/main/java/hu/dtits/springmvc/models/CategoryDTO.java
@@ -1,5 +1,6 @@
 package hu.dtits.springmvc.models;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CategoryDTO {
 
+    @ApiModelProperty(value = "Category ID", required = true)
     private Long id;
 
+    @ApiModelProperty(value = "Category Name", required = true)
     private String name;
 }
